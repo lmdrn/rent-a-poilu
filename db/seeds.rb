4018936d4127510d35a16dd924c5801a718adb51
Lea Medrano <leamedrano@gmail.com>
2020-02-18 09:51:16 +0100

    extension image + destro seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Poilu.destroy_all
+
 Poilu.create!(name: 'chat', description: 'chat poilu tout dou', location: 'Lausanne', user_id: 1)
 Poilu.create!(name: 'pantouffles', description: 'pantouffles poilues roses', location: 'Berne', user_id: 1)
 Poilu.create!(name: 'Yeti', description: 'monstre des montagnes blanc', location: 'Verbier', user_id: 1)
