810244e4b966f79acdb1cf54e7733136580f833f
Nicoleta Dudau <dudaunicoleta@yahoo.com>
2020-02-19 15:09:25 +0100

    delete poilu with bookings

--- app/models/poilu.rb
+++ app/models/poilu.rb
@@ -2,7 +2,7 @@ class Poilu < ApplicationRecord
   LOCATIONS = ['Lausanne','Renens','Genève','Bern','Zürich','Neuchâtel','Fribourg']
 
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   has_one_attached :photo
   validates :name, presence: true, uniqueness: true
