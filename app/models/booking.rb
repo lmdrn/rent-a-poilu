bddaa0a865330ea16dbcb2651125230ecf88ee12
michele Reymond <hello@michelereymond.ch>
2020-02-21 11:37:37 +0100

    calendrier terminé

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,13 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    if end_date < start_date
+      errors.add(:end_date, "la date de fin ne doit pas être antérieur à la date de début")
+    end
+  end
 end
